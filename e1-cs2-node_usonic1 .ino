03676df7f467fcb3e83120ee26b61749f3643027
ScriptWorkz <71452734+ssalims@users.noreply.github.com>
2021-03-31 11:58:04 +0800

    Add files via upload

---
+++ e1-cs2-node_usonic1 .ino
@@ -0,0 +1,39 @@
+// defines pins numbers esp32
+
+const int trigPin = 5;  //D5
+const int echoPin = 4;  //D4
+
+// defines variables
+long duration;
+int distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(2000);
+
+}
